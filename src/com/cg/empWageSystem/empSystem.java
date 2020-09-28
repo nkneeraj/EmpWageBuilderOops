7f05a33eae5b47e7a5a1652a9c90bff246580d02
nkneeraj <nkneerj641@gmail.com>
2020-09-29 02:40:22 +0530

    [NK]Added UC9: Saving the total wage for each company.

--- src/com/cg/empWageSystem/empSystem.java
+++ src/com/cg/empWageSystem/empSystem.java
@@ -5,10 +5,22 @@ import java.util.Random;
 public class empSystem {
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
 
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		int empHrs = 0, totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
+	public empSystem(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
 
+	public void computeEmpWage() {
+
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 
@@ -30,13 +42,19 @@ public class empSystem {
 			System.out.println("Day : " + totalWorkingDays + "empHrs : " + empHrs);
 		}
 		totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for Company: " + company + " is: " + totalEmpWage);
-		return totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Emp Wage for Company: " + company + " is: " + totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage("Reliance", 15, 25, 100);
-		computeEmpWage("BigBazar", 20, 27, 90);
-
+		empSystem reliance = new empSystem("Reliance", 15, 25, 100);
+		empSystem bigBazar = new empSystem("BigBazar", 20, 27, 90);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
+		bigBazar.computeEmpWage();
+		System.out.println(bigBazar);
 	}
 }
