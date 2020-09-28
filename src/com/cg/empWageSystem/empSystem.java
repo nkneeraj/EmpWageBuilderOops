af94e65a91e976151b45068313eabe811b208144
nkneeraj <nkneerj641@gmail.com>
2020-09-29 02:26:29 +0530

    [NK]Added UC2: Employee daily wage calculated.

--- src/com/cg/empWageSystem/empSystem.java
+++ src/com/cg/empWageSystem/empSystem.java
@@ -2,6 +2,18 @@ package com.cg.empWageSystem;
 
 public class empSystem {
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Employee Wage System");
+		int IS_FULL_TIME =1;
+		int EMP_RATE_PER_HOUR = 20;
+		
+		int empHrs = 0;
+		int empWage = 0;
+		
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			empHrs=8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
 	}
 }
